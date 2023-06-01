5ef63b9b47320ded00615f58c390b34c9f86018b
Danilo Siervi <danilosiervidepaula16@gmail.com>
2023-06-01 17:46:26 -0300

    Inserting and Contains functions on AVL Tree

--- avl_tree/src/lib.rs
+++ avl_tree/src/lib.rs
@@ -1,14 +1,16 @@
-type Link<T> = Option<Box<Node<T>>>;
+use std::cmp::Ordering;
+
+type Tree<T> = Option<Box<Node<T>>>;
 
 struct Node<T: Ord> {
     value: T,
     height: usize,
-    left: Link<T>,
-    right: Link<T>,
+    left: Tree<T>,
+    right: Tree<T>,
 }
 
 pub struct AvlTree<T: Ord> {
-    root: Link<T>,
+    root: Tree<T>,
     length: usize,
 }
 
@@ -16,4 +18,62 @@ pub struct AvlTree<T: Ord> {
 enum Side {
     Left,
     Right,
+}
+
+impl<T: Ord> AvlTree<T> {
+    pub fn new() -> Self {
+        AvlTree {
+            root: None,
+            length: 0,
+        }
+    }
+
+    pub fn contains(&self, value: &T) -> bool {
+        let mut current = &self.root;
+
+        while let Some(node) = current {
+            current = match value.cmp(&node.value) {
+                Ordering::Equal => return true,
+                Ordering::Less => &node.left,
+                Ordering::Greater => &node.right,
+            }
+        }
+
+        false
+    }
+
+    pub fn insert(&mut self, value: T) -> bool {
+        let inserted = insert(&mut self.root, value);
+
+        if inserted {
+            self.length += 1;
+        }
+
+        inserted
+    }
+}
+
+fn insert<T: Ord>(tree: &mut Tree<T>, value: T) -> bool {
+    if let Some(node) = tree {
+        let inserted = match value.cmp(&node.value) {
+            Ordering::Equal => false,
+            Ordering::Less => insert(&mut node.left, value),
+            Ordering::Greater => insert(&mut node.right, value),
+        };
+
+        if inserted {
+            node.rebalance();
+        }
+
+        inserted
+    } else {
+        *tree = Some(Box::new(Node {
+            value,
+            height: 1,
+            left: None,
+            right: None,
+        }));
+
+        true
+    }
 }
